12ec6a57f310f764467efb96b0f661bf8285810c
munzertopu <mmorshed@paradynamix.com>
2020-03-23 02:44:24 +0600

    done without destructuring

--- src/App.js
+++ src/App.js
@@ -1,55 +1,111 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rows: []
+    };
+
+    //   const movies = [
+    //     {
+    //       id: 0,
+    //       title: "Avengers : The Infinity War",
+    //       poster_src:
+    //         "https://image.tmdb.org/t/p/w185_and_h278_bestv2/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+    //       plot:
+    //         "As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. A despot of intergalactic infamy…"
+    //     },
+    //     {
+    //       id: 1,
+    //       title: "The Avengers",
+    //       poster_src:
+    //         "https://image.tmdb.org/t/p/w185_and_h278_bestv2/or06FN3Dka5tukK1e9sl16pB3iy.jpg",
+    //       plot: "the second plot"
+    //     }
+    //   ];
+
+    //   movies.forEach(movie => {
+    //     console.log(movie.title);
+    //   });
+  }
+
+  componentDidMount() {
+    this.getMovies("batman");
+  }
+
+  getMovies(searchTerm) {
+    const movieAPI =
+      "https://api.themoviedb.org/3/search/movie?api_key=3824ca0ab0ed2c88932d7a8379d969ad&language=en-US&page=1&include_adult=false&query=" +
+      searchTerm;
+    axios
+      .get(movieAPI)
+
+      .then(res => {
+        const results = res.data.results;
+
+        console.log(results);
+        var movieRows = [];
+
+        results.forEach(movie => {
+          movie.poster_src =
+            "https://image.tmdb.org/t/p/w185_and_h278_bestv2" +
+            movie.poster_path;
+          console.log(movie.title);
+          console.log(movie.poster_src);
+
+          const movieRow = (
+            <table key={movie.id}>
+              <tbody>
+                <tr>
+                  <td>
+                    <img width="120" src={movie.poster_src} alt="poster" />
+                  </td>
+                  <td>
+                    {movie.title}
+                    <p>{movie.overview}</p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          );
+          movieRows.push(movieRow);
+        });
+
+        this.setState({ rows: movieRows });
+      });
+  }
+
+  searchChangeHandler(event) {
+    console.log(event.target.value);
+    const searchTerm = event.target.value;
+    this.getMovies(searchTerm);
+  }
+
   render() {
     return (
-      <div classNameName="App">
-        {/* <div classNameName="table-responsive">
-          <table classNameName="table">
-            <tbody>
-              <tr>
-                <td>
-                  <img width="50" src="the-movie-db.svg" alt="" />
-                </td>
-
-                <td>Movie DB S</td>
-              </tr>
-            </tbody>
-          </table>
-        </div> */}
-
+      <div className="">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <a className="navbar-brand" href="/#">
             <img width="50" src="the-movie-db.svg" alt="" />
+            <span style={{ marginLeft: "1rem" }}>The Movie DB</span>
           </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-                <a className="nav-link" href="/#">
-                  The Movie DB
-                </a>
-              </li>
-            </ul>
-          </div>
         </nav>
 
-        <div class="form-group">
-          <input class="form-control input-lg" id="inputlg" type="text" />
+        <div className="form-group">
+          <input
+            onChange={this.searchChangeHandler.bind(this)}
+            className="form-control input-lg"
+            placeholder="Search The movie here"
+            type="text"
+          />
         </div>
+
+        {this.state.rows}
       </div>
     );
   }
---
+++ src/movieRow.js
@@ -0,0 +1,23 @@
+// import React from "react";
+
+// class movieRow extends React.Component {
+//   render() {
+//     return (
+//       <table key={this.props.movie.id}>
+//         <tbody>
+//           <tr>
+//             <td>
+//               <img width="120" src={this.props.movie.poster_src} alt="poster" />
+//             </td>
+//             <td>
+//               {this.props.movie.title}
+//               <p>{this.props.movie.plot}</p>
+//             </td>
+//           </tr>
+//         </tbody>
+//       </table>
+//     );
+//   }
+// }
+
+// export default movieRow;
