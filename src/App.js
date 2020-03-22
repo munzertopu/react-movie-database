d094a0b1c54b4197418e5c471c0b4616625803cc
munzertopu <mmorshed@paradynamix.com>
2020-03-22 21:07:32 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div classNameName="App">
+        {/* <div classNameName="table-responsive">
+          <table classNameName="table">
+            <tbody>
+              <tr>
+                <td>
+                  <img width="50" src="the-movie-db.svg" alt="" />
+                </td>
+
+                <td>Movie DB S</td>
+              </tr>
+            </tbody>
+          </table>
+        </div> */}
+
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="/#">
+            <img width="50" src="the-movie-db.svg" alt="" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <a className="nav-link" href="/#">
+                  The Movie DB
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <div class="form-group">
+          <input class="form-control input-lg" id="inputlg" type="text" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
